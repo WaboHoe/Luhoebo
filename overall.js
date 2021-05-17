66dc41626045089ebfb43006314ddafaf2b09111
backupWONG <77199407+backupWONG@users.noreply.github.com>
2021-05-17 16:50:10 +0800

    Add files via upload

---
+++ overall.js
@@ -0,0 +1,17 @@
+//Scrolling effect
+window.addEventListener('scroll',()=>{
+    var content=document.querySelector('.ABOUT');
+    var contentPlace=content.getBoundingClientRect().top;
+    var screenPosition=window.innerHeight /4;
+
+    if(contentPlace<screenPosition){
+        content.classList.add('active');
+    }
+    else{
+        content.classList.remove('active');
+    }
+
+});
+
+
+
